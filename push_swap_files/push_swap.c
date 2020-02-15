e8a28f51e79c37b6208484fcded0fd500a0d5d86
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-02-15 13:10:05 +0200

    adding sort_test

--- push_swap_files/push_swap.c
+++ push_swap_files/push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vkurkela <vkurkela@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 10:14:05 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/02/14 14:13:42 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/02/15 13:06:46 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,11 @@ int	main(int arg, char **argc)
 
 	sort_stack(&root_a, &root_b);
 	//ft_printf("2 round:\n");
-	//ft_printf("stack b:\n");
-	//print_stack(root_b);
+	ft_printf("stack b:\n");
+	print_stack(root_b);
 
-	//ft_printf("stack a:\n");
-	//print_stack(root_a);
+	ft_printf("stack a:\n");
+	print_stack(root_a);
 	free_stack(root_a);
 	return (0);
 }
--- push_swap_files/sort_stack.c
+++ push_swap_files/sort_stack.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sort_stack.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vkurkela <vkurkela@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/14 10:24:07 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/02/14 10:37:44 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/02/15 13:08:51 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,21 +39,43 @@ void	sort_stack_3(t_stack **root_a)
 		swap(root_a);
 }
 
+void	sort_stack_test(t_stack **root_a, t_stack **root_b, int size, int mid)
+{
+	int i;
+	int pivot;
+
+	i = 0;
+	pivot = mid;
+	while (i < size)
+	{
+		if ((*root_a)->data > pivot)
+			push_ab(root_a, root_b);
+		else
+			rotate(root_a);
+		i++;
+	}
+}
+
 void	sort_stack(t_stack **root_a, t_stack **root_b)
 {
 	t_stack *current;
 	int		size;
+	int		mid;
 
 	current = *root_a;
 	size = 0;
+	mid = 0;
 	while (current)
 	{
+		mid += current->data;
 		current = current->next;
 		size++;
 	}
+	mid /= size;
 	if (size == 2 && (*root_a)->data > (*root_a)->next->data)
 		rotate(root_a);
 	else if (size == 3)
 		sort_stack_3(root_a);
-	*root_b = NULL;
+	else
+		sort_stack_test(root_a, root_b, size, mid);
 }
