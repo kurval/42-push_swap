26f2118b35d09023279903fbe21640656ee0309e
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-02-16 11:54:07 +0200

    adding separate print functions

--- push_swap_files/rev_rotate.c
+++ push_swap_files/rev_rotate.c
@@ -1,45 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   swap.c                                             :+:      :+:    :+:   */
+/*   rev_rotate.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/16 10:44:43 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/02/16 11:46:06 by vkurkela         ###   ########.fr       */
+/*   Created: 2020/02/16 11:51:07 by vkurkela          #+#    #+#             */
+/*   Updated: 2020/02/16 11:51:39 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-static void	print_swap(t_stack *root)
+static void	print_rev_rotate(t_stack *root)
 {
 	if (!root->both)
-		root->stack == 'a' ? write(1, "sa\n", 3) :\
-		write(1, "sb\n", 3);
+		root->stack == 'a' ? write(1, "rra\n", 4) :\
+		write(1, "rrb\n", 4);
 	else
-	{
-        root->both == 1 ? write(1, "ss\n", 3) : 0;
+    {
+        root->both == 1 ? write(1, "rrr\n", 4) : 0;
         root->both = 0;
     }
 }
 
-void	swap(t_stack **root)
+void	reverse_rotate(t_stack **root)
 {
-	int temp;
+	t_stack *second_last;
+	char	c;
 
 	if (is_empty(*root) || is_empty((*root)->next))
 		return ;
-	print_swap(*root);
-	temp = (*root)->data;
-	(*root)->data = (*root)->next->data;
-	(*root)->next->data = temp;
+    print_rev_rotate(*root);
+	c = (*root)->stack == 'a' ? 'a' : 'b';
+	second_last = *root;
+	while (second_last->next->next != NULL)
+		second_last = second_last->next;
+	push(root, second_last->next->data, c);
+	del_node(&second_last->next);
 }
 
-void	swap_both(t_stack **root_a, t_stack **root_b)
+void	rev_rotate_both(t_stack **root_a, t_stack **root_b)
 {
 	(*root_a)->both = 1;
 	(*root_b)->both = 2;
-	swap(root_a);
-	swap(root_b);
+	reverse_rotate(root_a);
+	reverse_rotate(root_b);
 }
--- push_swap_files/rotate.c
+++ push_swap_files/rotate.c
@@ -6,11 +6,20 @@
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 10:43:33 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/02/16 11:17:39 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/02/16 11:51:47 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+static void	print_rotate(t_stack *root)
+{
+	if (!root->both)
+		root->stack == 'a' ? write(1, "ra\n", 3) :\
+		write(1, "rb\n", 3);
+	else if (root->both == 1)
+        write(1, "rr\n", 3);
+}
 
 void	rotate(t_stack **root)
 {
@@ -21,11 +30,7 @@ void	rotate(t_stack **root)
 
 	if (is_empty(*root) || is_empty((*root)->next))
 		return ;
-	if (!(*root)->both)
-		(*root)->stack == 'a' ? write(1, "ra\n", 3) :\
-		write(1, "rb\n", 3);
-	else if ((*root)->both == 1)
-        write(1, "rr\n", 3);
+    print_rotate(*root);
 	c = (*root)->stack == 'a' ? 'a' : 'b';
 	first = (*root)->data;
 	last = new_node(first, c);
@@ -44,35 +49,4 @@ void	rotate_both(t_stack **root_a, t_stack **root_b)
 	(*root_b)->both = 2;
 	rotate(root_a);
 	rotate(root_b);
-}
-
-void	reverse_rotate(t_stack **root)
-{
-	t_stack *second_last;
-	char	c;
-
-	if (is_empty(*root) || is_empty((*root)->next))
-		return ;
-	if (!(*root)->both)
-		(*root)->stack == 'a' ? write(1, "rra\n", 4) :\
-		write(1, "rrb\n", 4);
-	else
-    {
-        (*root)->both == 1 ? write(1, "rrr\n", 4) : 0;
-        (*root)->both = 0;
-    }
-	c = (*root)->stack == 'a' ? 'a' : 'b';
-	second_last = *root;
-	while (second_last->next->next != NULL)
-		second_last = second_last->next;
-	push(root, second_last->next->data, c);
-	del_node(&second_last->next);
-}
-
-void	rev_rotate_both(t_stack **root_a, t_stack **root_b)
-{
-	(*root_a)->both = 1;
-	(*root_b)->both = 2;
-	reverse_rotate(root_a);
-	reverse_rotate(root_b);
 }
