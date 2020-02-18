778fe597327de7760d59d3cb4a31e9a504fab365
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-02-18 20:32:53 +0200

    optimize

--- push_swap_files/big_sort.c
+++ push_swap_files/big_sort.c
@@ -6,7 +6,7 @@
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 21:56:19 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/02/18 18:28:51 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/02/18 20:32:26 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,7 @@ static void move_partitions(t_stack **root_a, t_stack **root_b, int pivot, int *
 			rotate(root_a);
 	}
     push_ab(root_a, root_b);
+    *size -= 1;
     (*root_b)->tag = 1;
     (*root_b)->chunk = partition;
 }
@@ -58,11 +59,15 @@ void	quick_sort(t_stack **root_a, t_stack **root_b, int size)
     int chunk;
 
     chunk = 0;
-    while (!is_empty(*root_a))
+    while (!is_empty(*root_a) && size > 3)
     {
         pivot = (*root_a)->data;
         chunk += 1;
         move_partitions(root_a, root_b, pivot, &size, chunk);
     }
+    if (size == 3)
+        sort_three(root_a);
+    else if (size == 2)
+        ((*root_a)->data > (*root_a)->next->data) ? swap(root_a) : 0;
     sort_b(root_a, root_b);
 }
